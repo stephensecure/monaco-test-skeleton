14100ed899cf81c581e5f0c8c539c91649fcc0a9
Paul Grimmer <paul.grimmer@monacosolicitors.co.uk>
2021-04-19 13:41:30 +0100

    Simple Fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    // Example call to the Hackernews API.
+    // Feel free to delete
+    fetch("https://hacker-news.firebaseio.com/v0/item/8863.json")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  });
+
+  return <div className="App">TODO: Monaco Solicitors React Coding Test</div>;
 }
 
 export default App;
